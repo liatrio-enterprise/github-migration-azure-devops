aa81e215283f845a7b755e231a87fd45206cbd78
Calvin Wai <cwai@MacBook-Pro.local>
2022-01-19 16:08:29 -0800

    feat: add import feature for repos

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as nodeApi from 'azure-devops-node-api';
 import * as GitApi from 'azure-devops-node-api/GitApi';
 import * as GitInterfaces from 'azure-devops-node-api/interfaces/GitInterfaces';
-import {Octokit} from "@octokit/rest";
+import { Octokit } from "@octokit/rest";
 
 require('dotenv').config()
 
@@ -11,31 +11,41 @@ async function getADORepos(adoOrgURL: string, adoToken: string, projectName: str
     return gitApiObject.getRepositories(projectName)
 }
 
+async function createGitHubRepos(repos: GitInterfaces.GitRepository[], org: string, github_pat: string, azure_pat: string) {
+    const octokit = new Octokit({ auth: github_pat })
 
-
-async function createGitHubRepos(repos: GitInterfaces.GitRepository[], org: string, token: string) {
-    const octokit = new Octokit({auth: token})
-
-    let createRepo = function (repoName: string){
+    let createRepo = async function (repoName: string, vsc_url: string) {
         console.log(`Creating ${repoName} in ${org}`)
-        octokit.rest.repos.createInOrg({
+        const result = await octokit.rest.repos.createInOrg({
             org: org,
             name: repoName,
             allow_merge_commit: true,
             allow_rebase_merge: false
-        }).catch(reason => console.log(reason))
+        });
+
+        console.log(`Starting import for ${result!?.data.name}`)
+        const importResult = await octokit.rest.migrations.startImport({
+            owner: org,
+            repo: result!?.data.name,
+            vcs: "git",
+            vcs_password: azure_pat,
+            vcs_url: vsc_url
+        })
+
+        console.log(`Finished importing - status: ${importResult.status}`);
     }
 
     for (const repo of repos) {
         console.log(`Checking for ${repo.name} in ${org}`)
 
-        let response = await octokit.rest.repos.get({
+        const response = await octokit.rest.repos.get({
             owner: org,
             repo: repo.name!
         }).catch(reason => {
             console.log(`${repo.name} not found in ${org} (got ${reason.status})`)
-            if(reason.status === 404){
-                createRepo(repo.name!)
+            if (reason.status === 404) {
+                // console.log(repo);
+                createRepo(repo.name!, repo.remoteUrl!)
             }
         });
 
@@ -48,7 +58,7 @@ async function createGitHubRepos(repos: GitInterfaces.GitRepository[], org: stri
 
 async function run() {
     let repos = await getADORepos(process.env.ADO_ORG!, process.env.AZURE_PERSONAL_ACCESS_TOKEN!, "kingfisher-demo")
-    await createGitHubRepos(repos, process.env.GITHUB_ORG!, process.env.GITHUB_TOKEN!)
+    await createGitHubRepos(repos, process.env.GITHUB_ORG!, process.env.GITHUB_TOKEN!, process.env.AZURE_PERSONAL_ACCESS_TOKEN!)
 }
 
 let promise = run();
