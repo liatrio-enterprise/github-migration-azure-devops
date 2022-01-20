2026c21dc87bf462dde81b5177c773f295a2ff8c
Steven O <steveno@liatrio.com>
2022-01-20 10:13:29 -0600

    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as nodeApi from 'azure-devops-node-api';
 import * as GitApi from 'azure-devops-node-api/GitApi';
 import * as GitInterfaces from 'azure-devops-node-api/interfaces/GitInterfaces';
-import { Octokit } from "@octokit/rest";
+import {Octokit} from "@octokit/rest";
 
 require('dotenv').config()
 
@@ -12,27 +12,38 @@ async function getADORepos(adoOrgURL: string, adoToken: string, projectName: str
 }
 
 async function createGitHubRepos(repos: GitInterfaces.GitRepository[], org: string, github_pat: string, azure_pat: string) {
-    const octokit = new Octokit({ auth: github_pat })
+    const octokit = new Octokit({auth: github_pat})
 
     let createRepo = async function (repoName: string, vsc_url: string) {
         console.log(`Creating ${repoName} in ${org}`)
-        const result = await octokit.rest.repos.createInOrg({
+        const creationResult = await octokit.rest.repos.createInOrg({
             org: org,
             name: repoName,
             allow_merge_commit: true,
             allow_rebase_merge: false
+        }).catch(reason => {
+            console.log('Repo creation failed:')
+            console.log(reason)
         });
+        if (!creationResult){
+            return;
+        }
+        console.log(`Creation complete for ${repoName} in ${org} (status: ${creationResult.status})`)
 
-        console.log(`Starting import for ${result!?.data.name}`)
+        console.log(`Starting import for ${creationResult!?.data.name}`)
         const importResult = await octokit.rest.migrations.startImport({
             owner: org,
-            repo: result!?.data.name,
+            repo: creationResult!?.data.name,
             vcs: "git",
             vcs_password: azure_pat,
             vcs_url: vsc_url
-        })
-
-        console.log(`Finished importing - status: ${importResult.status}`);
+        }).catch(reason => {
+            console.log('Repo importation failed:')
+            console.log(reason)
+        });
+        if (importResult){
+            console.log(`Finished importing ${repoName} in ${org} (status: ${importResult.status})`)
+        }
     }
 
     for (const repo of repos) {
@@ -56,9 +67,7 @@ async function createGitHubRepos(repos: GitInterfaces.GitRepository[], org: stri
     }
 }
 
-async function run() {
+(async () => {
     let repos = await getADORepos(process.env.ADO_ORG!, process.env.AZURE_PERSONAL_ACCESS_TOKEN!, "kingfisher-demo")
     await createGitHubRepos(repos, process.env.GITHUB_ORG!, process.env.GITHUB_TOKEN!, process.env.AZURE_PERSONAL_ACCESS_TOKEN!)
-}
-
-let promise = run();
+})()
